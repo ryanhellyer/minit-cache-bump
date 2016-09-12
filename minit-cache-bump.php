a35ff420827a62e4264656cd3c5e79d843382c0f
Ryan Hellyer <ryanhellyer@gmail.com>
2016-09-12 17:48:16 +0200

    Switching to hourly cache update to avoid excessive overflow of files that need deleted

--- minit-cache-bump.php
+++ minit-cache-bump.php
@@ -26,16 +26,13 @@ class Minit_Cache_Bump {
 		// Hook the cache purge
 		add_action( 'minit_cron_cache_bump', array( $this, 'cache_bump' ) );
 
-		// Add new WP Cron schedule
-		add_filter( 'cron_schedules', array( $this, 'cron_schedule' ) );
-
 	}
 
 	/*
 	 * Activate Cron job
 	 */
 	public static function activate() {
-		wp_schedule_event( time(), 'once_per_minute', 'minit_cron_cache_bump' );
+		wp_schedule_event( time(), 'hourly', 'minit_cron_cache_bump' );
 	}
 
 	/*
@@ -52,24 +49,6 @@ class Minit_Cache_Bump {
 
 		do_action( 'minit-cache-version-bump' );
 
-	}
-
-	/**
-	 * Adds a new cron interval to the WordPress scheduled tasks.
-	 *
-	 * Method adapted from code by Tom McFarlin (https://tommcfarlin.com/new-wordpress-cron-schedule/)
-	 *
-	 * @param  array $schedules The array of schedules that WordPress provides.
-	 * @return array The updated schedules with the weekly interval defined.
-	 */
-	public function cron_schedule( $schedules ) {
-
-		$schedules['once_per_minute'] = array(
-			'interval' => 60,
-			'display'  => 'Once per minute'
-		);
-
-		return $schedules;
 	}
 
 }
